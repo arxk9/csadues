d65ff0aed26104b32b6f7fb190924990f3591f32
arxk9 <az4xfp@virginia.edu>
2021-09-09 22:02:42 -0400

    hotfix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, request
 app = Flask("CSADues")
 
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
